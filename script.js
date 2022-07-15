b1af5e0d29fde3da0c5a163c7fe4655d52011065
Krish Shah <shahkrish2016@gmail.com>
2022-07-15 15:58:36 -0400

    added collision detection

--- script.js
+++ script.js
@@ -5,7 +5,7 @@ let canvas = document.querySelector('#game')
 canvas.width = 0.8*window.innerWidth;
 canvas.height = 0.8*window.innerHeight;
 
-// dynamically size window whenever necessary
+// dynamically size window whenever necess  ary
 window.addEventListener('resize', function(){
   canvas.width = 0.8*window.innerWidth;
   canvas.height = 0.8*window.innerHeight; 
@@ -21,14 +21,15 @@ let generateRandomInt = function(min, max) {
 class Person {
 
   static borderWidth = 0;
-  static seniorSpeed = 2;
+  static seniorSpeed = 1;
   static adultSpeed = Person.seniorSpeed * 1.5;
   static childSpeed = Person.adultSpeed * 1.5; 
   static steps = [-1, 1]
 
-  constructor(id_number, age_group) {
+  constructor(id_number, age_group, infected) {
     this.id_number = id_number;
     this.age_group = age_group;
+    this.infected = infected;
 
     // change radius as per dot type
     if (age_group === 'child') {
@@ -44,17 +45,18 @@ class Person {
       this.backgroundColor = '#bab21a';
       this.speed = Person.seniorSpeed;
     }
+
+    if (this.infected) {
+      this.backgroundColor = '#FF6666';
+    }
+
     this.borderColor = this.backgroundColor;
 
-    // 0, 100 are just temporary dummy values
+    // add validation for spawn area, to make sure don't spawn out of the screen 
     this.x = generateRandomInt(this.radius+1, canvas.width-this.radius-1);
     this.y = generateRandomInt(this.radius+1, canvas.height-this.radius-1);
 
     // set step size of Person object, different speed according to type of person
-    // this.stepX = this.speed;
-    // this.stepY = this.speed;
-    // console.log(generateRandomInt(0, 2))
-    // console.log(Person.steps[generateRandomInt(0, 2)] * this.speed)
     this.stepX = Person.steps[generateRandomInt(0, 2)] * this.speed;
     this.stepY = Person.steps[generateRandomInt(0, 2)] * this.speed;
   }
@@ -93,6 +95,11 @@ class Person {
     this.y += this.stepY;
   }
   
+  static intersect(a, b) {
+    let distance = Math.sqrt(((a.x-b.x) ** 2) + ((a.y-b.y) ** 2));
+    return distance <= a.radius + b.radius;
+  }
+  
 }
 
 id_counter = 1
@@ -100,21 +107,55 @@ id_counter = 1
 let dots = []
 let ages = ['child', 'adult', 'senior']
 
-for (let i = 0; i < 40; i++) {
-  dots.push(new Person(id_counter, ages[i % 3]));
+for (let i = 0; i < 10; i++) {
+  dots.push(new Person(id_counter, ages[i % 3], false));
   dots[i].draw();
   id_counter++;
 }
 
-// add infected dot(s)
+let current_infected = new Set();
 
-// add validation for spawn area, to make sure don't spawn out of the screen 
+// adding infected people
+dots.push(new Person(id_counter, "child", true));
+current_infected.add(id_counter);
+id_counter++;
 
 let updateCanvas = function() {
   requestAnimationFrame(updateCanvas);
   context.clearRect(0, 0, canvas.width, canvas.height);
+
+  let collisionOccurred = false;
+
+  // collision detection
+  for (let i = 0; i < dots.length; i++) {
+    for (let j = 0; j < dots.length; j++) {
+      if (i == j) continue;
+      if (Person.intersect(dots[i], dots[j])) {
+        // don't count situation where both are infected
+        if (dots[i].infected && dots[j].infected) {
+          continue
+        }
+        if (dots[i].infected || dots[j].infected) {
+          // differentiate between who is being infected, factor probabilities in
+
+          dots[i].backgroundColor = dots[i].borderColor = '#FF6666';
+          dots[j].backgroundColor = dots[j].borderColor = '#FF6666';
+          dots[i].infected = dots[j].infected = true;
+          current_infected.add(dots[i].id_number);
+          current_infected.add(dots[j].id_number);
+
+          collisionOccurred = true;
+        }
+      }
+    }
+  }
+
+  if (collisionOccurred) {
+    console.log("Percentage Infected: " + Math.round((current_infected.size)/dots.length * 100) + "%");
+    current_infected_count = current_infected.size;
+  }
+
   dots.forEach((currentPerson) => {
-    // console.log(currentPerson.id_number);
     currentPerson.update();
   });
 }
