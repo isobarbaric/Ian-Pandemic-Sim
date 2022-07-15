63e0270cfac38d2e53569aacc0990b6ad5b68fe0
Krish Shah <shahkrish2016@gmail.com>
2022-07-15 13:27:31 -0400

    added centered canvas for game

---
+++ script.js
@@ -0,0 +1,10 @@
+
+const canvas = document.querySelector('#game')
+const context = canvas.getContext('2d')
+
+// resize canvas according to screen size
+canvas.width = 0.6*window.innerWidth;
+canvas.height = 0.8*window.innerHeight;
+
+
+console.log('hello world')
